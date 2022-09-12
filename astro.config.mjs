41db062d755ff684d4930a09f98eb66c53868168
Leo Kling <leo.kling.info@gmail.com>
2022-09-12 11:45:05 +0200

    removed minfier as there was no effect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
-
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  vite: {
-    plugins: [threeMinifier()],
-  },
 });
