b5057a45d30a50dd3b92562cf24976c1896ddb26
Leo Kling <leo.kling.info@gmail.com>
2022-09-12 11:40:41 +0200

    added minifier for three?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
+
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [threeMinifier()],
+  },
 });
